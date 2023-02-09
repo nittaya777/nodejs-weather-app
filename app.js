9b093c56930734b6691f3a7d081cc0212e33082c
Nittaya Thaengthong <nittaya.may31@gmail.com>
2023-02-09 17:18:32 +0700

    config

---
+++ app.js
@@ -0,0 +1,24 @@
+const request = require("postman-request");
+const yargs = require("yargs");
+const hideBin = require("yargs/helpers");
+const geocode = require("./utils/geocode.js");
+const forecast = require("./utils/forecast.js");
+
+const address = process.argv[2];
+
+if (!address) {
+  console.log("Please provide an address");
+} else {
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(location);
+      console.log(forecastData);
+    });
+  });
+}
